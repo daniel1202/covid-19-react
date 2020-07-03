21fefde74bf89e8dec6b078b2b0d988d7f6a6655
Daniel Kamiński <daniel.kaminski1998@gmail.com>
2020-07-03 22:05:43 +0200

    title

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import { Cards, Chart, Countries } from './Components';
 import styles from './App.module.css';
 import { getData } from './api';
-import { createMuiTheme, Paper } from '@material-ui/core';
+import { createMuiTheme, Typography } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core';
 import Switch from '@material-ui/core/Switch';
 const App = () => {
@@ -30,6 +30,9 @@ const App = () => {
   return (
     <div className={styles.container}>
       <ThemeProvider theme={customTheme}>
+        <Typography variant='h1' color='textPrimary' className={styles.title}>
+          COVID -19
+        </Typography>
         <Countries handleCountryChange={handleCountryChange} />
         <Chart data={data} country={country} />
         <Cards data={data} />
